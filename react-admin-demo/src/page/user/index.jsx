b2b96a0eadb128f8ed923e80ced5c22412654729
LoadChange <soocto@gmail.com>
2018-06-30 22:40:51 +0800

    fix: 用户列表数据填充

--- react-admin-demo/src/page/user/index.jsx
+++ react-admin-demo/src/page/user/index.jsx
@@ -5,19 +5,37 @@ import {Layout, Table, Button, Pagination} from 'element-react';
 import PageTitle from 'component/page-title/index.jsx';
 
 const columns = [
-    {label: "ID", prop: "id", width: 80, fixed: 'left'},
-    {label: "用户名", prop: "username", width: 100},
-    {label: "邮箱", prop: "email", width: 120},
-    {label: "手机号", prop: "phone", width: 100},
-    {label: "创建时间", prop: "createTime", width: 200},
-    {label: "修改时间", prop: "updateTime", width: 200}
+    {label: "ID", prop: "id", width: 90, fixed: 'left'},
+    {label: "用户名", prop: "username", width: 140},
+    {label: "邮箱", prop: "email", width: 220},
+    {label: "手机号", prop: "phone", width: 140},
+    {label: "安全问题", prop: "question", width: 140},
+    {label: "答案", prop: "answer", width: 140},
+    {label: "创建时间", prop: "createTime", width: 180},
+    {label: "修改时间", prop: "updateTime", width: 180},
+    {
+        label: "操作",
+        prop: "zip",
+        fixed: 'right',
+        width: 100,
+        render: () => {
+            return (
+                <div>
+                    <Button type="text" size="small">查看</Button>
+                    <Button type="text" size="small">编辑</Button>
+                </div>
+            )
+        }
+    }
 ]
+
+const pageSize = 10
 
 class User extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            data: []
+            pageNum: 1
         }
     }
 
@@ -25,33 +43,34 @@ class User extends React.Component {
         this._getUserList()
     }
 
-    _getUserList() {
-        UserList().then(res => {
-            console.log(res)
-        })
+    _getUserList(pageNum) {
+        UserList({
+            pageSize,
+            pageNum: pageNum || this.state.pageNum
+        }).then(res => this.setState(Object.assign({}, res.data)))
+    }
+
+    onCurrentChange(pageNum) {
+        if (pageNum === this.state.pageNum) return
+        this._getUserList(pageNum)
     }
 
     render() {
         return (
-            <div className="user-list">
+            <div>
                 <PageTitle title="首页"/>
-                <Layout.Row>
-                    <Layout.Col span="24">
-                        <div className="container">
-                            <div className="list">
-                                <Table
-                                    style={{width: '100%'}}
-                                    columns={columns}
-                                    border={true}
-                                />
-                            </div>
-                            <div className="page">
-                                <Pagination layout="prev, pager, next, jumper" total={1000} pageSize={100}
-                                            currentPage={5}/>
-                            </div>
-                        </div>
-                    </Layout.Col>
-                </Layout.Row>
+                <div className="user-list">
+                    <Table
+                        style={{width: '100%'}}
+                        columns={columns}
+                        data={this.state.list}
+                        border={true}
+                    />
+                    <div className="page">
+                        <Pagination layout="prev, pager, next, jumper" onCurrentChange={this.onCurrentChange.bind(this)}
+                                    total={this.state.total} pageSize={pageSize} currentPage={this.state.pageNum}/>
+                    </div>
+                </div>
             </div>
         )
     }
