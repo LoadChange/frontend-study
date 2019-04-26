787892f455a9e0ef9157e056eafd83fb7805ce84
LoadChange <soocto@gmail.com>
2019-04-26 23:41:25 +0800

    🙅‍♀️ 引入session

--- nodejs/005-dev/app.js
+++ nodejs/005-dev/app.js
@@ -2,6 +2,14 @@ const querystring = require('querystring');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+const SESSION_DATA = {};
+
+const getCookeExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  return d.toGMTString();
+};
+
 const getPostData = req => {
   return new Promise((resolve, reject) => {
     if (req.method !== 'POST') {
@@ -21,27 +29,52 @@ const getPostData = req => {
         resolve({});
         return;
       }
-      console.log(postData);
       resolve(JSON.parse(postData));
     });
   });
 };
 
 const serverHandle = (req, res) => {
-  const { url } = req;
+  const { url, headers } = req;
   const [path, _query] = url.split('?');
   req.path = path;
   req.query = querystring.parse(_query);
+  const { cookie = '' } = headers;
+  req.cookie = {};
+  cookie.split(';').forEach(item => {
+    const [key = '', val = ''] = item.split('=');
+    req.cookie[key.trim()] = val.trim();
+  });
+
+  let needSetCookie = false;
+  let { userId } = req.cookie;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {};
+    }
+  } else {
+    needSetCookie = true;
+    userId = `${Date.now()}_${Math.random()}`;
+    SESSION_DATA[userId] = {};
+  }
+  req.session = SESSION_DATA[userId];
+
   res.setHeader('Content-type', 'application/json');
   getPostData(req).then(postData => {
     req.body = postData;
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
+      if (needSetCookie) {
+        res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookeExpires()}`);
+      }
       blogResult.then(blogData => res.end(JSON.stringify(blogData)));
       return;
     }
     const userResult = handleUserRouter(req, res);
     if (userResult) {
+      if (needSetCookie) {
+        res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookeExpires()}`);
+      }
       userResult.then(userData => res.end(JSON.stringify(userData)));
       return;
     }
--- nodejs/005-dev/src/router/user.js
+++ nodejs/005-dev/src/router/user.js
@@ -1,18 +1,28 @@
-const { loginCheck } = require('../controller/user');
+const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 const handleUserRouter = (req, res) => {
   const { method, url, path } = req;
-  if (method === 'POST' && path === '/api/user/login') {
-    const results = loginCheck(req.body);
+  if (method === 'GET' && path === '/api/user/login') {
+    // const results = login(req.body);
+    const results = login(req.query);
     if (results) {
       return results.then(user => {
         if (user.username) {
+          req.session.user = user;
           return new SuccessModel(user);
         }
         return new ErrorModel('登录失败');
       });
     }
+  }
+
+  if (method === 'GET' && path === '/api/user/login-test') {
+    console.log('req.session',req.session) 
+    if (req.session) {
+      return Promise.resolve(new SuccessModel(req.session));
+    }
+    return Promise.resolve(new ErrorModel('尚未登录'));
   }
 };
 
