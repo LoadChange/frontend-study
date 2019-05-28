7b1bbec74cf135ea6c1097a89f0642541dfe7a4c
LoadChange <soocto@gmail.com>
2019-05-28 23:57:21 +0800

    feat: 功能流程和文件结构

--- simple-music-player/renderer.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require("electron");
-window.addEventListener("DOMContentLoaded", () => {
-  ipcRenderer.send("message", "hello from renderer");
-  ipcRenderer.on("reply", (event, arg) => {
-    document.getElementById("message").innerHTML = arg;
-  });
-});
