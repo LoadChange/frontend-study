42c5fec6b66fa5f3c9f659698e192faa25600d77
LoadChange <soocto@gmail.com>
2019-06-18 23:41:08 +0800

    feat: graphql 定义类型

--- graphql/express/index.js
+++ graphql/express/index.js
@@ -3,13 +3,30 @@ const { buildSchema } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 
 const schema = buildSchema(`
+    type Account {
+        name: String
+        age: Int
+        sex: String
+        department: String
+    }
     type Query {
-        hello: String
+        hello: String,
+        accountName: String,
+        age: Int,
+        account: Account
     }
 `);
 
 const rootValue = {
   hello: () => 'hello world!',
+  accountName: () => 'apollo',
+  age: () => 18,
+  account: () => ({
+    name: 'apollo',
+    age: 18,
+    sex: '男',
+    department: 'school'
+  }),
 };
 
 const app = express();
