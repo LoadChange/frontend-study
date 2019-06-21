d0e1a6b5746246490d51ab6369c803d7ec2720d3
LoadChange <soocto@gmail.com>
2019-06-21 22:55:14 +0800

    feat: graphql 参数

--- graphql/express/index.js
+++ graphql/express/index.js
@@ -2,18 +2,25 @@ const express = require('express');
 const { buildSchema } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 
+/**
+ * 基本类型
+ * String Int Float Boolean ID
+ * [类型] 代表数组
+ */
 const schema = buildSchema(`
     type Account {
         name: String
         age: Int
         sex: String
         department: String
+        salary(city: String): Int
     }
     type Query {
         hello: String,
         accountName: String,
         age: Int,
-        account: Account
+        account(username: String): Account
+        getClassMates(classNo: Int!): [String]
     }
 `);
 
@@ -25,8 +32,21 @@ const rootValue = {
     name: 'apollo',
     age: 18,
     sex: '男',
-    department: 'school'
+    department: 'school',
+    salary({ city }) {
+      if (['北京', '上海', '广州', '深圳'].includes(city)) {
+        return 10000;
+      }
+      return 5000;
+    },
   }),
+  getClassMates({ classNo }) {
+    const obj = {
+      31: ['张三', '李四', '王五'],
+      61: ['张二三', '李二四', '王二五'],
+    };
+    return obj[classNo];
+  },
 };
 
 const app = express();
